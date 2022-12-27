de3fe4605a109611fa963e8aa8a06fd181dfb00e
Tom Gregor <tgregor@ivsoftware.com>
2022-12-27 07:15:49 -0700

    Add project files.

---
+++ sound-player-00/MainForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace sound_player_00
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(26, 33);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(112, 34);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 135);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "MainForm";
+            this.Text = "Player";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button buttonPlay;
+    }
+}
---
+++ sound-player-00/MainForm.cs
@@ -0,0 +1,38 @@
+
+
+using System.Diagnostics;
+using System.Media;
+
+namespace sound_player_00
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            buttonPlay.Click += onPlay;
+        }
+        private SoundPlayer _player = new SoundPlayer();
+        private void onPlay(object sender, EventArgs e)
+        {
+            var resource =
+                GetType()
+                .Assembly
+                .GetManifestResourceNames()
+                .FirstOrDefault(_ => _.Contains("hello-world.wav"));
+
+            if (resource == null)
+            {
+                Debug.Assert(false, "Resource not found.");
+            }
+            else
+            {
+                using (var stream = GetType().Assembly.GetManifestResourceStream(resource))
+                {
+                    _player.Stream = stream;
+                    _player.Play();
+                }
+            }
+        }
+    }
+}
